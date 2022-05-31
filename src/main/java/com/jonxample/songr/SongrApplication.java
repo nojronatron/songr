3ef967bb1f83365b3b0dbb6bbf96a97ec589339e
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-31 13:48:13 -0700

    initialized repo

---
+++ src/main/java/com/jonxample/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.jonxample.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
