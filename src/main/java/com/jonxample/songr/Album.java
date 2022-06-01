afbb474f18788249f192a984a7230b13cce5f114
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-31 20:19:58 -0700

    got th: attributes working, test built and passing

--- src/main/java/com/jonxample/songr/Album.java
+++ src/main/java/com/jonxample/songr/Album.java
@@ -6,4 +6,14 @@ public class Album {
     public int songCount;
     public float length; // in seconds
     public String imageUrl;
+
+    public Album(String albumTitle, String artistName,
+                 int albumSongCount, float albumSecondsLength,
+                 String albumArtUrl) {
+        this.title = albumTitle;
+        this.artist = artistName;
+        this.songCount = albumSongCount;
+        this.length = albumSecondsLength;
+        this.imageUrl = albumArtUrl;
+    }
 }
--- src/test/java/com/jonxample/songr/SongrApplicationTests.java
+++ src/test/java/com/jonxample/songr/SongrApplicationTests.java
@@ -2,12 +2,32 @@ package com.jonxample.songr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SongrApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testAlbumModelConstructor() {
+		String expectedTitle = "myTitle";
+		String expectedArtist = "myArtist";
+		int expectedSongCount = 8;
+		float expectedLength = 200.567f;
+		String expectedArtUrl = "./images";
+
+		Album sut = new Album(
+				expectedTitle,
+				expectedArtist,
+				expectedSongCount,
+				expectedLength,
+				expectedArtUrl
+		);
+
+		assertEquals(expectedTitle, sut.title, "Instantiated album should have the correct title");
+		assertEquals(expectedArtist, sut.artist, "Instantiated album should have the correct artist");
+		assertEquals(expectedSongCount, sut.songCount, "Instantiated album should have the correct song count");
+		assertEquals(expectedLength, sut.length, "Instantiated album should have the correct album length");
+		assertEquals(expectedArtUrl, sut.imageUrl, "Instantiated album should have the correct art path");
 	}
 
 }
