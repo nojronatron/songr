a09e84f49930827075d3a59260627bb2f3be3bd7
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-31 17:09:26 -0700

    added Album class and albums template page.

---
+++ src/main/java/com/jonxample/songr/Album.java
@@ -0,0 +1,9 @@
+package com.jonxample.songr;
+
+public class Album {
+    public String title;
+    public String artist;
+    public int songCount;
+    public float length; // in seconds
+    public String imageUrl;
+}
