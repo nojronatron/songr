64c4bb52192fdbbb75f3fb06b559b58c1b613b93
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-01 20:05:07 -0700

    completed setup and updated Album to be an Entity. DB not wired up yet.

---
+++ src/main/java/com/jonxample/songr/repositories/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.jonxample.songr.repositories;
+import com.jonxample.songr.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// from docs.spring.io: ...it takes the domain class to manage as well as
+// the id TYPE of the domain class as type arguments.
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+    public Album findByName(String name);
+}
